470e002a98b1a39305e46b616f2bfd2916872f7e
Nicholas Walker <Nicholas.Walker@warwick.ac.uk>
2023-04-29 19:30:18 +0100

    removed bad sanity chekcs

--- op2/src/gpi/op_gpi_rt_support.cpp
+++ op2/src/gpi/op_gpi_rt_support.cpp
@@ -114,10 +114,6 @@ void op_gpi_exchange_halo(op_arg *arg, int exec_flag){
             int ack_rank = wait_id & ((1<<NOTIF_SHIFT)-1); /* Filter out the upper half */
             int ack_dat_index= (int) wait_id>> NOTIF_SHIFT; /* Get only the upper half*/
             
-            //Sanity check
-            if(ack_dat_index!=dat->index || ack_rank !=exp_nonexec_list->ranks[i]){
-                GPI_FAIL("Accepted incorrect ack notification from unexpected dat.\n Expected: dat %d, rank %d got: dat %d, rank%d\n",dat->index, exp_exec_list->ranks[i], ack_dat_index, ack_rank);
-            }
 
             GPI_SAFE(gaspi_notify_reset(
                 EEH_SEGMENT_ID + gpi_buf->is_dynamic*DYNAMIC_SEG_ID_OFFSET, /* local segment */
@@ -196,11 +192,6 @@ void op_gpi_exchange_halo(op_arg *arg, int exec_flag){
             int ack_rank = wait_id & ((1<<NOTIF_SHIFT)-1); /* Filter out the upper half */
             int ack_dat_index= (int) wait_id>> NOTIF_SHIFT; /* Get only the upper half*/
             
-            //Sanity check
-            if(ack_dat_index!=dat->index || ack_rank !=exp_nonexec_list->ranks[i]){
-                GPI_FAIL("Accepted incorrect ack notification from unexpected dat.\n Expected: dat %d, rank %d got: dat %d, rank%d\n",dat->index, exp_nonexec_list->ranks[i], ack_dat_index, ack_rank)
-            }
-
             GPI_SAFE(gaspi_notify_reset(
                 ENH_SEGMENT_ID + gpi_buf->is_dynamic*DYNAMIC_SEG_ID_OFFSET, /* local segment */
                 wait_id,
